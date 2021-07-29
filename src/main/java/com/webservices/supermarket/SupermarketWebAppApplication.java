9b893a9ae1fc3215343d351b17614d5804285762
JhonLudenaReyes <ludena.jhon.j.6392@outlook.com>
2021-07-29 11:41:52 -0500

    Initial Project

---
+++ src/main/java/com/webservices/supermarket/SupermarketWebAppApplication.java
@@ -0,0 +1,13 @@
+package com.webservices.supermarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SupermarketWebAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SupermarketWebAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/webservices/supermarket/SupermarketWebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.webservices.supermarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SupermarketWebAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
